87e0cc925821a93b22a67f9681a027b2db5e780f
BimaShyii <bimashiita16@gmail.com>
2021-12-07 12:30:37 +0700

    Update app.py

--- app.py
+++ app.py
@@ -6,7 +6,7 @@ import cv2
 
 app = Flask(__name__)
 app.config['UPLOAD_FOLDER'] = './static/uploads/'
-model = load_model('elephant_lion_class_model.h5')
+model = load_model('kemangi_seledri_class_model.h5')
 
 class_dict = {0: 'Elephant (Gajah)', 1: 'Lion (Singa)'}
 
