e31e822199fc313f11c4e7cf9054a10e4215dcd0
BimaShyii <bimashiita16@gmail.com>
2021-12-07 12:34:28 +0700

    second commit

--- app.py
+++ app.py
@@ -8,7 +8,7 @@ app = Flask(__name__)
 app.config['UPLOAD_FOLDER'] = './static/uploads/'
 model = load_model('kemangi_seledri_class_model.h5')
 
-class_dict = {0: 'Daun Kemangi (Kemangi)', 1: 'Daun Seledri (Seledri)'}
+class_dict = {0.5: 'Daun Kemangi (Kemangi)', 1: 'Daun Seledri (Seledri)'}
 
 def predict_label(img_path):
     query = cv2.imread(img_path)
